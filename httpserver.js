4f9cf12ed8bb25e4670d76e7bca80b4852210f08
maro <pmaroti@gmail.com>
2017-09-17 20:38:41 +0200

    under test

--- httpserver.js
+++ httpserver.js
@@ -1,18 +1,15 @@
-
-
-function dhtRead() {
-  if (!n) n=10;
+function dhtRead(pin,cb) {
   var d = "";
   var ht = this;
-  pinMode(ht.pin); // set pin state to automatic
-  digitalWrite(ht.pin, 0);
-  this.watch = setWatch(function(t) {
+  pinMode(pin); // set pin state to automatic
+  digitalWrite(pin, 0);
+  var watch = setWatch(function(t) {
     d+=0|(t.time-t.lastTime>0.00005);
-  }, ht.pin, {edge:'falling',repeat:true} );
-  setTimeout(function() {pinMode(ht.pin,'input_pullup');},1);
+  }, pin, {edge:'falling',repeat:true} );
+  setTimeout(function() {pinMode(pin,'input_pullup');},1);
   setTimeout(function() {
-    clearWatch(ht.watch);
-    delete ht.watch;
+    clearWatch(watch);
+    delete watch;
     var cks =
         parseInt(d.substr(2,8),2)+
         parseInt(d.substr(10,8),2)+
@@ -24,12 +21,9 @@ function dhtRead() {
         rh : parseInt(d.substr(2,8),2),
         temp : parseInt(d.substr(18,8),2)
       });
-    } else {
-      if (n>1) setTimeout(function() {ht.read(cb,--n);},500);
-      else cb({err:true, checksumError:cks>0, raw:d, temp:-1, rh:-1});
     }
   }, 50);
-};
+}
 
 function onPageRequest(req, res) { 
     var a = url.parse(req.url, true);
@@ -41,18 +35,12 @@ function onPageRequest(req, res) {
     }
 
     if(a.path == "/temperature") {
-        if(dhtloaded) {
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            var dht=require("DHT11").connect(D14);
-            dht.read(
-                function (a) {
-                    res.end('{ "temperature" : ' + a.temp.toString() + ', "humidity" :' + a.rh.toString()+'}');
-                }
-            );
-            return;
-        }
-        res.writeHead(503,{'Content-Type' : 'text/plain'});
-        res.end("Module not loaded yet.");
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        dhtRead(D14,
+            function (a) {
+                res.end('{ "temperature" : ' + a.temp.toString() + ', "humidity" :' + a.rh.toString()+'}');
+            }
+        );
         return;
     }
     
